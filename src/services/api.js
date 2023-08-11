9a92a8046953a4825965e943026b7fa2179297a1
gyujh630 <rbwjrbxo@ajou.ac.kr>
2023-08-11 14:48:31 +0900

    Feat: 유저기부정보 불러오기 api

--- src/services/api.js
+++ src/services/api.js
@@ -13,7 +13,7 @@ export const getApplicationList = async () => {
   }
 };
 
-//user token으로 user의 수혜신청목록 가져오기
+//user token으로 user의 수혜신청목록 가져오기 - endpoint 수정 필요
 export const getMyApplicationList = async (token) => {
   try {
     const response = await axios.get(`${apiUrl}/myApplications`, {
@@ -24,6 +24,21 @@ export const getMyApplicationList = async (token) => {
     return response.data;
   } catch (error) {
     console.error("Error fetching myApplication list:", error);
+    throw error;
+  }
+};
+
+//user의 기부목록 가져오기 - endpoint 수정 필요
+export const getDonationList = async () => {
+  try {
+    const response = await axios.get(`${apiUrl}/`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching user Donation list:", error);
     throw error;
   }
 };
