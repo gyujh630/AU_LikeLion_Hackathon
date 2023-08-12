1c4d1595e94930ebb6666b61de7b268111d2d4ea
yesolz <yesol@ajou.ac.kr>
2023-08-12 17:43:15 +0900

    feat: 로그인

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,67 +1,115 @@
-import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
-const Login = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+function Login() {
+  const formSchema = yup.object({
+    id: yup.string().required("아이디을 입력해주세요."),
+    password: yup.string().required("비밀번호를 입력해주세요."),
+  });
 
-  const handleUsernameChange = (e) => {
-    setUsername(e.target.value);
-  };
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors, isValid, isDirty },
+  } = useForm({
+    mode: "onChange",
+    resolver: yupResolver(formSchema),
+  });
 
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
+  const onSubmit = async (data) => {
+    try {
+      const response = await fetch("/users/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    // 로그인 폼 제출되었을 때 처리하는 로직 추가
-
-    console.log("Username:", username);
-    console.log("Password:", password);
+      if (response.ok) {
+        // 회원가입 성공 처리
+      } else {
+        // 회원가입 실패 처리
+      }
+    } catch (error) {
+      console.error("Error during sign up:", error);
+    }
   };
 
   return (
-    <div style={{ marginTop: "100px" }}>
+    <div className="login" style={{ marginTop: "100px" }}>
       <div style={styles.container}>
-        <h1>Login</h1>
-        <form onSubmit={handleSubmit} style={styles.form}>
+        <h1>로그인</h1>
+        <form onSubmit={handleSubmit(onSubmit)} style={styles.form}>
           <div style={styles.inputGroup}>
             <label htmlFor="username" style={styles.label}>
-              <span style={{ display: "inline-block", width: "100px" }}>
+              <span
+                style={{
+                  display: "inline-block",
+                  width: "100px",
+                  marginRight: "10px",
+                }}
+              >
                 아이디
               </span>
             </label>
             <input
-              type="text"
-              id="username"
-              value={username}
-              onChange={handleUsernameChange}
+              name="id"
+              placeholder="아이디"
+              {...register("id")}
               style={styles.input}
             />
+            {errors.id && (
+              <div
+                style={{ marginLeft: "100px", marginTop: "10px", color: "red" }}
+              >
+                {errors.id.message}
+              </div>
+            )}
           </div>
           <div style={styles.inputGroup}>
             <label htmlFor="password" style={styles.label}>
-              <span style={{ display: "inline-block", width: "100px" }}>
+              <span
+                style={{
+                  display: "inline-block",
+                  width: "100px",
+                  marginRight: "10px",
+                }}
+              >
                 비밀번호
               </span>
             </label>
             <input
               type="password"
-              id="password"
-              value={password}
-              onChange={handlePasswordChange}
+              name="password"
+              placeholder="비밀번호"
+              {...register("password")}
               style={styles.input}
             />
+            {errors.password && (
+              <div
+                style={{ marginLeft: "100px", marginTop: "10px", color: "red" }}
+              >
+                {errors.password.message}
+              </div>
+            )}
           </div>
-          <button type="submit" style={styles.button}>
-            Login
-          </button>
+          <input
+            type="submit"
+            disabled={!isValid} // Disable the button based on form validity
+            style={{
+              ...styles.button,
+              backgroundColor: isValid && isDirty ? "#4CAF50" : "#ccc",
+              cursor: isValid && isDirty ? "pointer" : "not-allowed",
+            }}
+          />
         </form>
       </div>
     </div>
   );
-};
+}
 
 const styles = {
   container: {
@@ -77,7 +125,7 @@ const styles = {
     marginTop: "20px",
   },
   inputGroup: {
-    marginBottom: "15px",
+    marginBottom: "40px",
   },
   label: {
     marginBottom: "5px",
