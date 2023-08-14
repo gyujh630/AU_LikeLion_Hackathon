5c5c8e03713dc8e62791682d137e639bde8a71a9
gyujh630 <rbwjrbxo@ajou.ac.kr>
2023-08-15 01:22:50 +0900

    Feat: 로그인 시 상단바 변경 구현

--- src/components/default/NavBar.js
+++ src/components/default/NavBar.js
@@ -1,7 +1,51 @@
 // NavBar.js
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import React from "react";
 import styled from "styled-components";
+
+const NavBar = () => {
+  const navigate = useNavigate();
+  const handleLogout = async () => {
+    // 로그아웃 처리 로직
+    // api 호출
+    // 성공 시 -> localStorage 토큰 삭제, 로그인 상태 업데이트
+    localStorage.removeItem("token");
+    navigate("/");
+  };
+
+  return (
+    <StyledNav>
+      <ul>
+        <li>
+          <Button to="/">Home</Button>
+        </li>
+        <li>
+          <Button to="/postlist">PostList</Button>
+        </li>
+        {/* login 여부에 따른 조건부 렌더링 */}
+        {localStorage.getItem("token") ? (
+          <>
+            <li>
+              <LogoutButton onClick={handleLogout}>Logout</LogoutButton>
+            </li>
+            <li>
+              <Button to="/mypage">MyPage</Button>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <Button to="/login">Login</Button>
+            </li>
+            <li>
+              <Button to="/signup">Sign Up</Button>
+            </li>
+          </>
+        )}
+      </ul>
+    </StyledNav>
+  );
+};
 
 const StyledNav = styled.nav`
   width: 100%;
@@ -49,28 +93,19 @@ const Button = styled(NavLink)`
   }
 `;
 
-const NavBar = () => {
-  return (
-    <StyledNav>
-      <ul>
-        <li>
-          <Button to="/">Home</Button>
-        </li>
-        <li>
-          <Button to="/postlist">PostList</Button>
-        </li>
-        <li>
-          <Button to="/login">Login</Button>
-        </li>
-        <li>
-          <Button to="/signup">Sign Up</Button>
-        </li>
-        <li>
-          <Button to="/mypage">MyPage</Button>
-        </li>
-      </ul>
-    </StyledNav>
-  );
-};
+const LogoutButton = styled(NavLink)`
+  text-decoration: none;
+  padding: 10px 20px;
+  border: 2px solid #333;
+  border-radius: 5px;
+  color: #333 !important;
+  font-weight: bold;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: #333;
+    color: white !important;
+  }
+`;
 
 export default NavBar;
