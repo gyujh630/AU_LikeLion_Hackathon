76ab13ee16d862697dc72153c815ae5c7fdc8ab4
gyujh630 <rbwjrbxo@ajou.ac.kr>
2023-08-16 03:15:49 +0900

    Feat: Footer link, logo, navbar scroll event

--- src/components/default/NavBar.js
+++ src/components/default/NavBar.js
@@ -1,11 +1,30 @@
 // NavBar.js
 import { NavLink, useNavigate } from "react-router-dom";
-import React from "react";
+import { useRef, useEffect } from "react";
 import styled from "styled-components";
 import { setLogOut, isLogin } from "../../constants/auth";
 
 const NavBar = () => {
-  const navigate = useNavigate();
+  const targetRef = useRef(null);
+  const handleScroll = () => {
+    console.log("scrolling");
+    if (window.scrollY > 50) {
+      targetRef.current.style.borderBottom = "0.5px solid #ececec";
+    } else {
+      targetRef.current.style.borderBottom = "";
+    }
+  };
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      window.addEventListener("scroll", handleScroll);
+    }, 100);
+    return () => {
+      clearInterval(timer);
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   const handleLogout = async () => {
     // 로그아웃 처리 로직
     // <api 호출 부분>
@@ -15,7 +34,8 @@ const NavBar = () => {
   };
 
   return (
-    <StyledNav>
+    <StyledNav ref={targetRef}>
+      <img id="logo" src={process.env.PUBLIC_URL + "/imgs/logo3.jpg"} />
       <ul>
         <li>
           <Button to="/">Home</Button>
@@ -53,12 +73,18 @@ const StyledNav = styled.nav`
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: #f0f8ff;
+  background-color: white;
   position: fixed; /* Set the navigation bar to a fixed position */
   top: 0; /* Stick the navigation bar to the top */
   left: 0;
   z-index: 1; /* Ensure the navigation bar is above other content */
   height: 80px; /* Increase the height of the navigation bar */
+
+  img#logo {
+    width: 100px;
+    position: absolute;
+    left: 30px;
+  }
 
   ul {
     display: flex;
@@ -74,6 +100,8 @@ const StyledNav = styled.nav`
     margin-right: 0;
   }
 `;
+
+//#468499
 
 const Button = styled(NavLink)`
   text-decoration: none;
